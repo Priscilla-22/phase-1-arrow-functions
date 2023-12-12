2a3414f228aec2b4a69ee197ad01d54069f6781a
Priscilla-22 <priscillamw123@gmail.com>
2023-12-12 14:48:07 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function () {
+  return 2000 / 100;
+};
+console.log(divide());
+
+let x = 2
+const square = x => x * x;
+console.log(square(x));
+
+let a = 3
+let b = 4
+const add = (a, b) => a + b;
+console.log(add(a, b));
